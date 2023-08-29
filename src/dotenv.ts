4041228183684ac85a01d314bd07968af553fdb8
MohamadKh75 <khajavi.contact@gmail.com>
2023-08-29 11:20:49 +0330

    chore: get isCI from user

--- src/dotenv.ts
+++ src/dotenv.ts
@@ -1,12 +1,12 @@
 import type { DotenvConfigOptions } from 'dotenv';
 import { config as loadEnv } from 'dotenv';
-import isCI from 'is-ci';
 
 /**
  * Merge environment variables from `.env` into Cypress config.
  *
  * @param config - Original Cypress config.
  * @param dotenvConfig - Configuration for `dotenv`.
+ * @param isCI - Flag indicating whether the code is running in a CI environment. Defaults to `process.env.CI`.
  *
  * @returns Updated Cypress config with merged env vars.
  *
@@ -14,7 +14,8 @@ import isCI from 'is-ci';
  */
 export const dotenv = (
   config: Cypress.PluginConfigOptions,
-  dotenvConfig?: DotenvConfigOptions
+  dotenvConfig?: DotenvConfigOptions,
+  isCI: boolean = Boolean(process.env.CI)
 ): Cypress.PluginConfigOptions => {
   if (isCI) return config;
 
